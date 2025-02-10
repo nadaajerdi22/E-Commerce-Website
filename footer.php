00c1b5bb83ed079859498cb9a69a55b782e35b0c
nadaajerdi22 <nadaajerdi2022@gmail.com>
2025-02-10 19:21:28 +0100

    First commit: Upload project

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="contact.php">contact</a>
+            <a href="shop.php">shop</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="login.php">login</a>
+            <a href="register.php">register</a>
+            <a href="orders.php">my orders</a>
+            <a href="cart.php">my cart</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +212 767-490069 </p>
+            <p> <i class="fas fa-phone"></i> +212 656-564950 </p>
+            <p> <i class="fas fa-envelope"></i> nadaajerdi291@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Oulmes, Morocco - 15102 </p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit">&copy; copyright @ <?php echo date('Y'); ?> by <span>Nada</span> </div>
+
+</section>
